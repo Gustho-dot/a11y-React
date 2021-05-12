9f14b00f911286abfc2ead9a2e64c2cbf54f6808
Thomas <karlthomas.gustafsson@gmail.com>
2021-05-12 18:25:44 +0200

    ADD: Setting up folder structure and components

---
+++ a11y-react/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Header;
