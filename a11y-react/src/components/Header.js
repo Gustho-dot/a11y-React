218b9cc72fa0f606a5e310eaacfe013621da6578
Thomas <karlthomas.gustafsson@gmail.com>
2021-05-13 15:41:32 +0200

    Setting up routes

--- a11y-react/src/components/Header.js
+++ a11y-react/src/components/Header.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 
 const Header = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/About">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/Contact">Contact</Link>
+                    </li>
+                </ul>
+            
+           
+
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/about">
+                    </Route>
+                    <Route path="/contact">
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default Header;
