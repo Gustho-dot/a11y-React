5dfcc9f4ee98931aa8924ab648a97f3ac9c8d0db
Thomas <karlthomas.gustafsson@gmail.com>
2021-05-17 16:23:23 +0200

    ADD: broke up text into component

--- a11y-react/src/routes/home.js
+++ a11y-react/src/routes/home.js
@@ -1,48 +1,12 @@
 import React from 'react';
+import TextSection from '../components/TextSection';
 
 const Home = () => {
     return (
-    <div>
-        <section class="textImage">
-        <img src="../images/computer.png" alt="computer" class="textImages" />
-        <h1>Rubrik 1</h1>
-        <p> The standard Lorem Ipsum passage, used since the 1500s
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna 
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-            dolor in reprehenderit in voluptate velit esse 
-            cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-        </section>
-        <section class="textImage">
-            <img src="../images/eagle.png" alt="eagle" class="textImages" />
-            <h2>Rubrik 2</h2>
-            <p> "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium 
-                voluptatum deleniti atque corrupti 
-                quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, 
-                similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
-                Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis
-                est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus,
-                omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut
-                    officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
-                Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores
-                alias consequatur aut perferendis doloribus asperiores repellat."</p>
-            </section>
-            <section class="textImage">
-                <img src="../images/tree.png" alt="tree" class="textImages" />
-                <h3>Rubrik 3</h3>
-                <p> The standard Lorem Ipsum passage, used since the 1500s
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna 
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-                    dolor in reprehenderit in voluptate velit esse 
-                    cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, 
-                    sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </section>
+        <div>
+            <TextSection image="../images/computer.png" rubrik="Rubrik 1" />
+            <TextSection image="../images/eagle.png" rubrik="Rubrik 2" />
+            <TextSection image="../images/tree.png" rubrik="Rubrik 3" />
         </div>
     )
 }
